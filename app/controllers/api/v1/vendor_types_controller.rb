217329c37397c093001316082a87c570b956176a
KristinHannah <kristinhannahk@gmail.com>
2020-03-19 17:13:31 -0400

    setting up

---
+++ app/controllers/api/v1/vendor_types_controller.rb
@@ -0,0 +1,2 @@
+class Api::V1::VendorTypesController < ApplicationController
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+vendor_type = VendorType.create(name: 'venue')
+
+vendor = Vendor.create(vendor_type_id: 1, name: 'Box House Hotel', location: 'Greenpoint, Brooklyn, NY', availability: 'September 18, 2020', quote: '195')
+
+vendor_two = Vendor.create(vendor_type_id: 1, name: 'Dobbin Street', location: 'Williamsburg, Brooklyn, NY', availability: 'November 14, 2020, October 31, 2020', quote: '220')
