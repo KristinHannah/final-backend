d652561a98c9551e48215f1202641dd9a7dbb575
KristinHannah <kristinhannahk@gmail.com>
2020-03-19 17:34:22 -0400

    building out controllers

--- app/controllers/api/v1/vendor_types_controller.rb
+++ app/controllers/api/v1/vendor_types_controller.rb
@@ -1,2 +1,34 @@
 class Api::V1::VendorTypesController < ApplicationController
+
+    def index 
+        @vendor_types = VendorType.all 
+        render json: @vendor_types
+
+    end 
+
+    def create 
+        @vendor_type = VendorType.new(vendor_type_params)
+        if @vendor_type.save 
+            render json: @vendor_type
+        else 
+            render json: {error: "Error creating vendor type" }
+        end 
+    end 
+
+    def show 
+        @vendor_type = VendorType.find(params[:id])
+        render json: @vendor_type
+    end 
+
+    def destroy 
+        @vendor_type = VendorType.find(params[:id])
+        @vendor_type.destroy 
+    end 
+
+    private 
+
+    def vendor_type_params
+        params.require(:vendor_type).permit(:name, :img)
+    end 
+
 end
--- app/controllers/api/v1/vendors_controller.rb
+++ app/controllers/api/v1/vendors_controller.rb
@@ -1,2 +1,39 @@
 class Api::V1::VendorsController < ApplicationController
+   before_action :set_vendor_type
+    
+    def index 
+        @vendors = @vendor_type.vendors 
+        render json: @vendor
+    end 
+
+    def create 
+        @vendor = Vendor.new(vendor_params)
+        if @vendor.save 
+            render json: @vendor
+        else 
+            render json: {error: "Error creating vendor type" }
+        end 
+    end 
+
+    def show 
+        @vendor = Vendor.find(params[:id])
+        render json: @vendor
+    end 
+
+    def destroy 
+        @vendor = Vendor.find(params[:id])
+        @vendor.destroy 
+    end 
+
+
+    private 
+
+    def set_vendor_type 
+        @vendor_type = VendorType.find(params[:vendor_type_id])
+    end 
+
+    def vendor_params
+        params.require(:vendor).permit(:name, :location, :availability, :quote, :vendor_type_id)
+    end 
+
 end
